9d30305db4aec442e9ff7dd5eb5141614824eddd
Niklas Duong <niklas.duong@gmail.com>
2017-06-14 14:27:15 +0200

    Working on the algorithm for the AI player.

--- src/main/java/GameLogic/Board.java
+++ src/main/java/GameLogic/Board.java
@@ -2,12 +2,22 @@
  * Created by eps on 2017-06-12.
  */
 package GameLogic;
-public class Board
-{
-private Slot[][] board;
+
+public class Board {
+    private Slot[][] board;
 
     public Board() {
-        board = new Slot[6][7] ;
+        board = new Slot[6][7];
+    }
+
+    public void copyBoard(Board board) {
+        Slot[][] newSlot = new Slot[6][7];
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 7; col++) {
+                newSlot[row][col] = board.getSlot(row, col);
+            }
+        }
+        this.setBoard(newSlot);
     }
 
     public Slot[][] getBoard() {
@@ -18,15 +28,15 @@ private Slot[][] board;
         this.board = board;
     }
 
-    public Slot getSlot(int row, int column){
+    public Slot getSlot(int row, int column) {
         return board[row][column];
     }
 
-    public void setSlot (Slot slot, int row, int column){
+    public void setSlot(Slot slot, int row, int column) {
         board[row][column] = slot;
     }
 
-    public void setSlotState (SlotState slotState, int row, int column) {
+    public void setSlotState(SlotState slotState, int row, int column) {
         board[row][column].setSlotState(slotState);
     }
 
