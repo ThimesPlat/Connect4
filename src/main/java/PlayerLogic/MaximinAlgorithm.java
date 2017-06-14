35d3edd71579aa7072e6cf4076f022e917cfe3a6
Niklas Duong <niklas.duong@gmail.com>
2017-06-14 10:35:27 +0200

    maximin
    Maximin algorithm

--- src/main/java/PlayerLogic/MaximinAlgorithm.java
+++ src/main/java/PlayerLogic/MaximinAlgorithm.java
@@ -1,7 +1,14 @@
 package PlayerLogic;
+import GameLogic.Board;
+
+import java.util.List;
 
 public class MaximinAlgorithm {
 	private int col, analyze, maxDepth;
-	
-	
+	private List<Integer> score;
+	private Board board;
+
+	public MaximinAlgorithm(){
+
+    }
 }
