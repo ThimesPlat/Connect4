62c1e8db94df2277f4fbc3e2c06613a4c0746e26
Kevin Tatou <kevin.tatou@sigmatechnology.se>
2017-06-20 10:34:22 +0200

    added minimaxtest

--- src/main/java/PlayerLogic/MiniMaxTest.java
+++ src/main/java/PlayerLogic/MiniMaxTest.java
@@ -1,21 +1,75 @@
 package PlayerLogic;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import GameLogic.*;
 
 public class MiniMaxTest {
 
+	Game game;
+	Slot redSlot;
+	Slot yellowSlot;
+	Slot emptySlot;
+	MiniMax miniMax;
+
+    @Mock
+    protected Slot mockYellowSlot = mock(Slot.class);
+    @Mock
+    protected Slot mockRedSlot = mock(Slot.class);
+    @Mock
+    protected static Slot mockEmptySlot = mock(Slot.class);
+
+    Player player;
+    Board board;
+    
+    protected  final Slot[][] testingBoardHorizontalThree = {
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockRedSlot,mockRedSlot,mockRedSlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot}
+        } ;
+    protected  final Slot[][] testingBoardVerticalThree = {
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockRedSlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockRedSlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockRedSlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot}
+        } ;
+    
+    protected  final Slot[][] testingBoardDiagonalThree = {
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockEmptySlot,mockRedSlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockEmptySlot,mockRedSlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot},
+            {mockRedSlot,mockEmptySlot,mockEmptySlot,mockEmptySlot, mockEmptySlot, mockEmptySlot, mockEmptySlot}
+        } ;
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
 	@Before
 	public void setUp() throws Exception {
+        when(mockYellowSlot.getSlotState()).thenReturn(SlotState.YELLOW);
+        when(mockRedSlot.getSlotState()).thenReturn(SlotState.RED);
+        when(mockEmptySlot.getSlotState()).thenReturn(SlotState.EMPTY);
+		game = new Game();
+		board = new Board();
+		miniMax = new MiniMax(board);
+		player = new Player(SlotState.RED);
 		
+
 	}
 
 	@After
@@ -23,9 +77,24 @@ public class MiniMaxTest {
 	}
 
 	@Test
-	public void test() {
+	public void testVertical() {
+		board.setBoard(testingBoardVerticalThree);
+		assertEquals(miniMax.eval(board, player), 1010);
 		
-		fail("Not yet implemented");
 	}
-
+	@Test
+	public void testHorizontal() {
+		board.setBoard(testingBoardHorizontalThree);
+		assertEquals(miniMax.eval(board, player), 3030);
+		
+	}
+	
+	
+	@Test
+	public void testDiagonal() {
+		board.setBoard(testingBoardDiagonalThree);
+		assertEquals(miniMax.eval(board, player), 2020);
+		
+	}
+	
 }
